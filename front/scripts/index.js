2c537a3e22e3cb168f815c94f66df97e0f8c681d
Mauri Pino Ricci <mauripino69@gmail.com>
2024-05-29 19:10:49 -0300

    hw boostrap

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -8,31 +8,31 @@ const renderCards = (data) => {
     data.forEach((movie) => {
 
         const containerCard = document.createElement('div');
-        containerCard.classList.add('card');
+        containerCard.classList.add('card-custom');
 
         const titleElement = document.createElement('a');
         titleElement.classList.add("card-title");
-        titleElement.innerHTML = `<span class="strong-text">Título :</span> ${movie.title}`;
+        titleElement.innerHTML = `<span class="strong-text">Título: </span> ${movie.title}`;
 
         const yearElement = document.createElement('p');
         yearElement.classList.add("card-text");
-        yearElement.innerHTML = `<span class="strong-text">Año :</span> ${movie.year}`;
+        yearElement.innerHTML = `<span class="strong-text">Año: </span> ${movie.year}`;
 
         const directorElement = document.createElement('h4');
         directorElement.classList.add("card-text");
-        directorElement.innerHTML = `<span class="strong-text">Director: :</span> ${movie.director}`;
+        directorElement.innerHTML = `<span class="strong-text">Director: </span> ${movie.director}`;
 
         const durationElement = document.createElement('p');
         durationElement.classList.add("card-text");
-        durationElement.innerHTML = `<span class="strong-text">Duración: :</span> ${movie.duration}`;
+        durationElement.innerHTML = `<span class="strong-text">Duración: </span> ${movie.duration}`;
 
         const genreElement = document.createElement('p');
         genreElement.classList.add("card-text");
-        genreElement.innerHTML = `<span class="strong-text">Género: :</span> ${movie.genre}`;
+        genreElement.innerHTML = `<span class="strong-text">Género: </span> ${movie.genre}`;
 
         const rateElement = document.createElement('p');
         rateElement.classList.add("card-text");
-        rateElement.innerHTML = `<span class="strong-text">Puntación: :</span> ${movie.rate}`;
+        rateElement.innerHTML = `<span class="strong-text">Puntación: </span> ${movie.rate}`;
 
         const imageElement = document.createElement('img');
         imageElement.classList.add("card-img");
