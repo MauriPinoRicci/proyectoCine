2c653e954fa20c5fb78ed06074549e5c4e69910d
Mauri Pino Ricci <mauripino69@gmail.com>
2024-06-12 20:47:37 -0300

    hw clase 6

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,18 @@
 const renderCards = require("./render_cards");
+const axios = require("axios");
 
-$.get("https://api.1rodemayo.com/movies/", (data) => {
-    renderCards(data)
-});
+const fetchDataAndRenderCards = async () => {
+    try {
+        const response = await axios.get("https://api.1rodemayo.com/movies/");
+        console.log("tamo bien");        
+        console.log(response.data);
+        const data = response.data;
+        renderCards(data); // Llama a la función renderCards con los datos obtenidos de la API
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
+fetchDataAndRenderCards();
+
+//queda pendiente hacer commit de la hw clase 6
